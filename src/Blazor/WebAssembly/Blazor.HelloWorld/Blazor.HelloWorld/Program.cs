ba5a34f31f4158513ac3c313a830950c78b00a98
qpooqp <qpooqp.dev@gmail.com>
2020-07-26 10:50:16 +0200

    Initial commit with single project (Simple WebAssembly Blazor application)

---
+++ src/Blazor/WebAssembly/Blazor.HelloWorld/Blazor.HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Blazor.HelloWorld
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder
+                .CreateDefault(args);
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder
+                .Build()
+                .RunAsync();
+        }
+    }
+}
